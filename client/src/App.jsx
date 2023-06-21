7d15377bfdb0b2d24e4fab2e69bbc2326bec88c5
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-06-21 03:11:00 +0100

    username

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Username from './components/Username'
+import Recovery from './components/Recovery'
+import Profile from './components/Profile'
+import Reset from './components/Reset'
+import PageNotFound from './components/PageNotFound'
+import Password from './components/Password'
+import Register from './components/Register'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+
+  },
+
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+
+  {
+    path: '/recovery',
+    element: <Profile></Profile>
+  },
+  
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>
+  },
+
+])
+
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl">
-        Login
-      </h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+  </main>
   )
 }
